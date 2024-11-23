3ca867841c817f8cae265b34a2c66b1727d36bb3
fpd.core20@gmail.com <fpd.core20@gmail.com>
2024-11-23 20:49:47 +0300

    Добавьте файлы проекта.

---
+++ OtusLinq/Extensions.cs
@@ -0,0 +1,36 @@
+﻿namespace OtusLinq
+{
+	public static class Extensions
+	{
+		public static IEnumerable<T> Top<T>(this IEnumerable<T> source, int percent) where T : IComparable<T>
+		{
+			if (percent < 1 || percent > 100)
+			{
+				throw new ArgumentException("Value must be between 1 and 100.", nameof(percent));
+			}
+
+			var sortedList = source.ToList();
+			sortedList.Sort((x, y) => y.CompareTo(x));
+
+			int countToTake = (int)Math.Ceiling(sortedList.Count * (percent / 100.0));
+
+			return sortedList.Take(countToTake);
+		}
+
+		public static IEnumerable<TSource> Top<TSource, TKey>(this IEnumerable<TSource> source,
+			int percent, Func<TSource, TKey> keySelector)
+		where TKey : IComparable<TKey>
+		{
+			if (percent < 1 || percent > 100)
+			{
+				throw new ArgumentException("Value must be between 1 and 100.", nameof(percent));
+			}
+
+			var sortedList = source.OrderByDescending(keySelector).ToList();
+
+			int countToTake = (int)Math.Ceiling(sortedList.Count * (percent / 100.0));
+
+			return sortedList.Take(countToTake);
+		}
+	}
+}
---
+++ OtusLinq/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace OtusLinq
+{
+	internal class Program
+	{
+		private static readonly List<int> _intList = new List<int>{1,2,3,4,5,6,7,8,9};
+		private static readonly List<Person> _persons = new List<Person>
+		{
+			new Person("bob", 1),
+			new Person("alice", 2),
+			new Person("name3", 3),
+			new Person("name4", 4),
+			new Person("name5", 5),
+			new Person("name6", 6),
+			new Person("name7", 7),
+			new Person("name8", 8),
+			new Person("name9", 9)
+		};
+		static void Main(string[] args)
+		{
+			var newIntList = _intList.Top(30);
+			foreach (int i in newIntList)
+			{
+				Console.WriteLine(i);
+			}
+			var newPersonList = _persons.Top(30, person => person.Age);
+			foreach(var person in newPersonList)
+			{
+				Console.WriteLine($"{person.Name} - {person.Age}");
+			}
+		}
+	}
+}
